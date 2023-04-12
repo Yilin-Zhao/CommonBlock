f42df9884f1db4e2a18823fa9c26d84e80488915
Yilin Zhao <zy2280@nyu.edu>
2023-04-12 12:22:52 -0400

    .

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = 'f69bab14e88d577f730e04b72a480acbad15d5ebdb038ad7'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@127.0.0.1/CommonBlock'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:YES@localhost/CommonBlock'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
